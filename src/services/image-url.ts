c35cbaa010715ddc92d16e1a8c87b8dbd3494414
lihao <lhao@anchnet.com>
2023-09-26 21:19:28 +0800

    handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,7 @@
+import noImage from "../assets/no-image-placeholder.jpg";
+
 const getCroppedImageURL = (url: string) => {
+  if (!url) return noImage;
   const index = url.indexOf("media/") + "media/".length;
   const cropped = url.slice(0, index) + "crop/600/400/" + url.slice(index);
   return cropped;

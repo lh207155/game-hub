7f40cc7b732a2c21018d2457ca6a8804bedb2369
lihao <lhao@anchnet.com>
2023-09-26 22:40:16 +0800

    implement searching

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-const Navbar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const Navbar = ({ onSearch }: Props) => {
   return (
     <HStack display="flex" alignItems="center" justifyContent="space-between">
       <Image src={logo} boxSize="60px" p={1}></Image>
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch></ColorModeSwitch>
     </HStack>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,15 +1,28 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import React from "react";
+import React, { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
-
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
   return (
-    <InputGroup>
-      <InputLeftElement>
-        <BsSearch />
-      </InputLeftElement>
-      <Input placeholder="Search games..." variant={"filled"} />
-    </InputGroup>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (ref.current) {
+          console.log(ref.current.value);
+          onSearch(ref.current.value);
+        }
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement>
+          <BsSearch />
+        </InputLeftElement>
+        <Input ref={ref} placeholder="Search games..." variant={"filled"} />
+      </InputGroup>
+    </form>
   );
 };
 
